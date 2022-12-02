39da6415fac7fbfbc3a1b6c29196cc5eab0b944e
Harash_1421 <abdulaziz.harash@hotmail.com>
2022-12-02 08:22:44 +0300

    ChangeAppearances

--- MultibleApperances/ViewController.swift
+++ MultibleApperances/ViewController.swift
@@ -1,19 +1,51 @@
-//
-//  ViewController.swift
-//  MultibleApperances
-//
-//  Created by Abd Harash on 6/17/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var main_tvApperances:UILabel!
+    @IBOutlet weak var maion_buChangeApperances:UIButton!
+    var loadApperance = UserDefaults.standard.string(forKey: "ApperanceMode")
+    var apperances = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        apperances = loadApperance!
+        print(apperances)
+        if(apperances == "Dark"){
+            overrideUserInterfaceStyle = .dark
+        }else{
+            overrideUserInterfaceStyle = .light
+        }
     }
-
-
+    
+    @IBAction func main_buChangeApperances(_ sender:Any){
+        showToastAction()
+    }
+    
+    //Show Toast Action
+    func showToastAction(){
+        let alert = UIAlertController(title:nil, message: "Chosse Apperances", preferredStyle: .alert)
+        let alertLight = UIAlertAction(title: "Light", style: .default) {action in
+            self.main_tvApperances.text = "Light Mode"
+            self.apperancesMode(apperances: "Light")
+        }
+        let alertDark = UIAlertAction(title: "Dark", style: .default){action in
+            self.main_tvApperances.text = "Dark Mode"
+            self.apperancesMode(apperances: "Dark")
+        }
+        alert.addAction(alertLight)
+        alert.addAction(alertDark)
+        self.present(alert, animated: true)
+    }
+    
+    //Function For Apperances Mode
+    func apperancesMode(apperances:String){
+        if(apperances == "Dark"){
+            overrideUserInterfaceStyle = .dark
+        }else{
+            overrideUserInterfaceStyle = .light
+        }
+        UserDefaults.standard.set(apperances, forKey: "ApperanceMode")
+    }
 }
 
